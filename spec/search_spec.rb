1e7d3b325ca2c776efb8ad60799fa5d36b037a12
Brent Vatne <brentvatne@gmail.com>
2012-07-05 15:14:11 -0700

    cleanup

--- spec/search_spec.rb
+++ spec/search_spec.rb
@@ -1,14 +1,11 @@
 require 'spec_helper'
 require 'yaml'
 
-class TranslationNotInThisFile; end
-
 describe 'testing' do
   let(:key) { "en.storefront.search.product_search_result.publisher" }
   let(:yml_files) { Dir[Dir.pwd + '/spec/translations/**/*.yml'] }
 
   it 'finds it' do
-    puts yml_files.inspect
     keys = key.split(/\./)
     match = nil
 
@@ -22,7 +19,9 @@ describe 'testing' do
         value = value[search_keys.shift]
       end
 
-      match = {:value => value, :key => keys.last, :file => yml_file, :line_numbers => []} unless value.nil?
+      unless value.nil?
+        match = {:value => value, :key => keys.last, :file => yml_file, :line_numbers => []}
+      end
     end
 
     File.open(match[:file]) do |f|
